42f2e4ea1761485805da8001f93adf3c48acf0fe
MrHatkeeper <kare.krch@educanet.cz>
2024-01-05 17:03:50 +0100

    chybí spuder a canFit

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
-import base
-import base as Base
-import copy, random, time, math
-from PIL import Image, ImageDraw
+import copy
+import glob
+
+import base as base
+import random
+import os
 
 
 # Player template for HIVE --- ALP semestral work
@@ -9,11 +11,11 @@ from PIL import Image, ImageDraw
 
 
 # PUT ALL YOUR IMPLEMENTATION INTO THIS FILE
-class Player(Base.Board):
+class Player(base.Board):
     def __init__(self, playerName, myIsUpper, size, myPieces, rivalPieces):  # do not change this line
-        Base.Board.__init__(self, myIsUpper, size, myPieces, rivalPieces)  # do not change this line
+        base.Board.__init__(self, myIsUpper, size, myPieces, rivalPieces)  # do not change this line
         self.playerName = playerName
-        self.algorithmName = "UngusBongus"
+        self.algorithmName = "💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩"
 
     def getAllEmptyCells(self):
         result = []
@@ -31,33 +33,14 @@ class Player(Base.Board):
                     result.append([p, q])
         return result
 
-    def isOnePiece(self) -> bool:
-        holder = self.getAllFigures()
-        cells = holder[0] + holder[1]
-
-        if len(cells) == 0:
-            return True
-
-        somethingChanged = True
-        frame = [cells[0]]
-        cells.pop(0)
-        while somethingChanged:
-            somethingChanged = False
-            for cell in cells:
-                neighbors = list(filter(lambda x: isOccupied(x, self.board), getAllNeighbor(cell)))
-                for neighbor in neighbors:
-                    if neighbor in frame:
-                        frame.append(cell)
-                        cells.remove(cell)
-                        somethingChanged = True
-                        break
-                if somethingChanged:
-                    break
-        return sorted(holder[1] + holder[0]) == sorted(frame)
-
     def canFit(self, actPos: list[int], nePos: list[int]) -> bool:
         # TODO do it lol
         return True
+
+    def isCellViable(self, pos: list[int]):
+        if isInBoard(self.size, pos[0], pos[1]) and self.board[pos[0]][pos[1]] == "":
+            return True
+        return False
 
     def queenMoves(self, pos: list[int]) -> list[list[int]]:
         moves = []
@@ -65,33 +48,38 @@ class Player(Base.Board):
 
         for direction in directions:
             newPos = [pos[0] + direction[0], pos[1] + direction[1]]
-            if isInBoard(self.size, newPos[0],
-                         newPos[1]) and self.board[newPos[0]][newPos[1]] == "" and self.canFit(pos, newPos):
+            if self.isCellViable(newPos) and self.canFit(pos, newPos):
                 moves.append(newPos)
         return moves
 
-    def bugMoves(self, position: list[int]) -> list[list[int]]:
-        pass
-
-    def antMoves(self, position: list[int]) -> list[list[int]]:
+    def bugMoves(self, pos: list[int]) -> list[list[int]]:
         moves = []
         directions = [(1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1)]
 
         for direction in directions:
-            newPos = [position[0] + direction[0], position[1] + direction[1]]
-            directionNeighbors = getAllNeighbor(newPos)
-            if list(direction) in directionNeighbors:
-                directionNeighbors.remove(list(direction))
-            if self.isCellViable(newPos) and self.canFit(position, newPos):
-                pass
-
+            newPos = [pos[0] + direction[0], pos[1] + direction[1]]
+            if isInBoard(self.size, newPos[0], newPos[1]) and self.canFit(pos, newPos):
+                moves.append(newPos)
         return moves
 
-    def isCellViable(self, newPos: list[int]):
-        return isInBoard(self.size, newPos[0], newPos[1]) and self.board[newPos[0]][newPos[1]][-1] == ""
+    def antMoves(self, position: list[int], prevMoves: list[list[int]]) -> list[list[int]]:
+        prevMoves.append(position)
+        moves = []
+        directions = [(1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1)]
+        for direction in directions:
+            newPos = [position[0] + direction[0], position[1] + direction[1]]
+            if newPos not in prevMoves:
+                directionNeighbors = list(filter(lambda x: isOccupied(x, self.board), getAllNeighbor(newPos)))
+                if position in directionNeighbors:
+                    directionNeighbors.remove(position)
+                if len(directionNeighbors) > 0 and self.isCellViable(newPos) and self.canFit(position, newPos):
+                    moves += [newPos]
+                    moves += self.antMoves(newPos, prevMoves)
+        return moves
 
-    def spiderMoves(self, position: list[int]) -> list[list[int]]:
-        pass
+    def spiderMoves(self, position: list[int], prevMoves: list[list[int]], distance: int) -> list[list[int]]:
+        # TODO
+        return [position, []]
 
     def grasshopperMoves(self, position: list[int]) -> list[list[int]]:
         moves = []
@@ -111,65 +99,102 @@ class Player(Base.Board):
             return self.grasshopperJump(position, nextPosition)
         return nextPosition
 
-    def getAllFigures(self) -> list[list[list[int]]]:
-        myFigures = []
-        enemyFigures = []
-        for p in self.board:
-            for q in self.board[p]:
-                if self.board[p][q] != "":
-                    if self.board[p][q][-1].islower():
-                        myFigures.append([p, q])
-                    elif self.board[p][q][-1].isupper():
-                        enemyFigures.append([p, q])
-        return [myFigures, enemyFigures]
-
-    def getAllLegalMoves(self) -> list[list[list[int]]]:
-        myFigures = self.getAllFigures()[0]
-        possibleMoves = []
+    def getAllLegalMoves(self) -> dict[tuple[int], list[list[int]]]:
+        player = 0
+        if self.myColorIsUpper:
+            player = 1
+        myFigures = getAllFigures(self.board)[player]
+        possibleMoves = dict[tuple, list[list[int]]]()
         for figure in myFigures:
-            match self.board[figure[0]][figure[1]][-1]:
+            match self.board[figure[0]][figure[1]][-1].lower():
                 case "b":
-                    possibleMoves.append([figure, self.bugMoves(figure)])
+                    possibleMoves[tuple(figure)] = self.bugMoves(figure)
                 case "a":
-                    possibleMoves.append([figure, self.antMoves(figure)])
+                    possibleMoves[tuple(figure)] = self.antMoves(figure, [])
                 case "g":
-                    possibleMoves.append([figure, self.grasshopperMoves(figure)])
+                    possibleMoves[tuple(figure)] = self.grasshopperMoves(figure)
                 case "s":
-                    possibleMoves.append([figure, self.spiderMoves(figure)])
+                    possibleMoves[tuple(figure)] = self.spiderMoves(figure, [], 0)
                 case "q":
-                    possibleMoves.append([figure, self.queenMoves(figure)])
-        # TODO check if OnePiece
-        for i in possibleMoves:
-            for j in i:
-                if [] in j:
-                    possibleMoves.remove(i)
-                    break
-        print(possibleMoves)
-        return possibleMoves
+                    possibleMoves[tuple(figure)] = self.queenMoves(figure)
+        output = dict[tuple, list[list[int]]]()
+        for possibleMove in possibleMoves.keys():
+            for newPosition in possibleMoves[possibleMove]:
+                if newPosition != [] and isOnePiece(self.board, list(possibleMove), newPosition):
+                    if not tuple(possibleMove) in output:
+                        output[tuple(possibleMove)] = []
+                    output[tuple(possibleMove)] += [newPosition]
+
+        return output
+
+    def placePiece(self) -> list:
+        insect = random.choice(list(self.myPieces.keys()))
+
+        emptyCells = self.getAllEmptyCells()
+        usableCells = []
+
+        for emptyCell in emptyCells:
+            neighbors = list(filter(lambda x: isOccupied(x, self.board), getAllNeighbor(emptyCell)))
+            if (len(neighbors)) > 0:
+                usableCells.append(emptyCell)
+
+        coord = random.choice(usableCells)
+
+        return [insect, None, None, coord[0], coord[1]]
+
+    def movePiece(self):
+        moves = self.getAllLegalMoves()
+        prevPosition = random.choice(list(moves.keys()))
+        newPosition = moves[prevPosition][random.randint(0, len(moves[prevPosition]) - 1)]
+        insect = self.board[prevPosition[0]][prevPosition[1]]
+        return [insect, prevPosition[0], prevPosition[1], newPosition[0], newPosition[1]]
 
     def move(self):
         """ return [animal, oldP, oldQ, newP, newQ], or [animal, None, None, newP, newQ] or [] """
+        allFigures = getAllFigures(self.board)
+        if len(allFigures[0] + allFigures[1]) == 0:
+            return ["s", None, None, 3, 6]
 
-        # the following code just randomly places (ignoring all the rules) some random figure at the board
-        emptyCells = self.getAllEmptyCells()
-
-        if len(emptyCells) == 0:
+        if len(self.getAllEmptyCells()) == 0:
             return []
 
-        randomCell = emptyCells[random.randint(0, len(emptyCells) - 1)]
-        randomP, randomQ = randomCell
+        if len(self.myPieces) > 0:
+            return self.placePiece()
 
-        for animal in self.myPieces:
-            if self.myPieces[animal] > 0:  # is this animal still available? if so, let's place it
-                return [animal, None, None, randomP, randomQ]
+        return self.movePiece()
 
-        # all animals are places, let's move some randomly (again, while ignoring all rules)
-        allFigures = self.getAllNonemptyCells()
-        randomCell = allFigures[random.randint(0, len(allFigures) - 1)]
-        randomFigureP, randomFigureQ = randomCell
-        # determine which animal is at randomFigureP, randomFigureQ
-        animal = self.board[randomFigureP][randomFigureQ][-1]  # [-1] means the last letter
-        return [animal, randomFigureP, randomFigureQ, randomP, randomQ]
+
+def moveFigure(board: dict[int, dict[int, str]], prevPos: list[int], newPos: list[int]) -> dict[int, dict[int, str]]:
+    newBoard = copy.deepcopy(board)
+    newBoard[newPos[0]][newPos[1]] = board[prevPos[0]][prevPos[1]]
+    newBoard[prevPos[0]][prevPos[1]] = board[prevPos[0]][prevPos[1]][:-1]
+    return newBoard
+
+
+def isOnePiece(board: dict[int, dict[int, str]], prevPos: list[int], newPos: list[int]) -> bool:
+    newBoard = moveFigure(board, prevPos, newPos)
+    holder = getAllFigures(newBoard)
+    cells = holder[0] + holder[1]
+
+    if len(cells) == 0:
+        return True
+
+    somethingChanged = True
+    frame = [cells[0]]
+    cells.pop(0)
+    while somethingChanged:
+        somethingChanged = False
+        for cell in cells:
+            neighbors = list(filter(lambda x: isOccupied(x, newBoard), getAllNeighbor(cell)))
+            for neighbor in neighbors:
+                if neighbor in frame:
+                    frame.append(cell)
+                    cells.remove(cell)
+                    somethingChanged = True
+                    break
+            if somethingChanged:
+                break
+    return sorted(holder[1] + holder[0]) == sorted(frame)
 
 
 def isInBoard(size: int, p: int, q: int) -> bool:
@@ -191,6 +216,19 @@ def isOccupied(pos: list[int], board: dict[int, dict[int, str]]) -> bool:
     if board[pos[0]][pos[1]] != "":
         return True
     return False
+
+
+def getAllFigures(board: dict[int, dict[int, str]]) -> list[list[list[int]]]:
+    myFigures = []
+    enemyFigures = []
+    for p in board:
+        for q in board[p]:
+            if board[p][q] != "":
+                if board[p][q][-1].islower():
+                    myFigures.append([p, q])
+                elif board[p][q][-1].isupper():
+                    enemyFigures.append([p, q])
+    return [myFigures, enemyFigures]
 
 
 def updatePlayers(move, activePlayer, passivePlayer):
@@ -225,17 +263,23 @@ if __name__ == "__main__":
                     "g": 2}  # key is animal, value is how many is available for placing
     bigFigures = {figure.upper(): smallFigures[figure] for figure in smallFigures}  # same, but with upper case
 
+    files = glob.glob("moves/*")
+    for f in files:
+        os.remove(f)
+
     P1 = Player("player1", False, 13, smallFigures, bigFigures)
     P2 = Player("player2", True, 13, bigFigures, smallFigures)
 
+    P1.board[0][0] = "g"
+    P1.board[0][1] = "A"
+    P1.board[1][1] = "G"
+    P1.board[0][2] = "a"
+    P2.board = copy.deepcopy(P1.board)
     filename = "moves/begin.png"
 
-    P1.board[3][6] = "q"
-
-    P1.getAllLegalMoves()
     P1.saveImage(filename)
 
-    """moveIdx = 0
+    moveIdx = 0
     while True:
 
         move = P1.move()
@@ -254,6 +298,6 @@ if __name__ == "__main__":
         P1.myMove = moveIdx
         P2.myMove = moveIdx
 
-        if moveIdx > 0:
+        if moveIdx > 4:
             print("End of the test game")
-            break"""
+            break
